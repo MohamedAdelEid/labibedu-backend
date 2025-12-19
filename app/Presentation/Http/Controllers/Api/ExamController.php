da09107ddea66279883622857ad2e77bc74d209c
MohamedAdelEid <01114979112mohamed@gmail.com>
2025-12-20 01:22:55 +0200

    fix: update pagination parameter in ExamController and correct question title in QuestionSeeder

--- app/Presentation/Http/Controllers/Api/ExamController.php
+++ app/Presentation/Http/Controllers/Api/ExamController.php
@@ -34,7 +34,7 @@ class ExamController extends Controller
         $studentId = auth()->user()->student->id;
         $perPage = $request->input('per_page', 10);
 
-        $data = $this->examFacade->getExamDetails($id, $studentId, 10);
+        $data = $this->examFacade->getExamDetails($id, $studentId, $perPage);
 
         return ApiResponse::success(
             new ExamDetailsResource($data),
